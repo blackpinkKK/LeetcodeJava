cc6f0ee1f60a8b1fb1a9274dd6a2609d3e39962d
DissMan <2450836123@qq.com>
2020-07-11 18:57:21 +0800

    Practice: leetcode 687. 最长同值路径

--- Tree/LongestUnivaluePath.java
+++ Tree/LongestUnivaluePath.java
@@ -12,13 +12,7 @@
 
 package LeetcodeJava.Tree;
 
-import LeetcodeJava.DynamicProgramming.MaxProduct;
 import LeetcodeJava.utils.TreeNode;
-
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
 
 public class LongestUnivaluePath {
 
