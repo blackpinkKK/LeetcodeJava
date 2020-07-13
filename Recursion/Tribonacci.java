c7060cfe52a1b9cc7c8ef9e9c87ac36a94aa96e9
DissMan <2450836123@qq.com>
2020-07-13 22:49:38 +0800

    Practice: leetcode 1137. 第 N 个泰波那契数

---
+++ Recursion/Tribonacci.java
@@ -0,0 +1,29 @@
+/**
+ * 1137. 第 N 个泰波那契数
+ */
+
+package LeetcodeJava.Recursion;
+
+public class Tribonacci {
+    public static void main(String[] args) {
+        int n = 25;
+        Tribonacci obj = new Tribonacci();
+        System.out.println(obj.tribonacci(n));
+    }
+
+    public int tribonacci(int n) {
+        if(n==0) return 0;
+        int t0 = 0;
+        int t1 = 1;
+        int t2 = 1;
+
+        for (int i = 3; i < n + 1; i++) {
+            int tmp = t0 + t1 + t2;
+            t0 = t1;
+            t1 = t2;
+            t2 = tmp;
+        }
+
+        return t2;
+    }
+}
